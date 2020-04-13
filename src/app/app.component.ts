806715d4874aea44da71c62cee62a3ebbe10209a
Janina Zylinska <janeczka@gmail.com>
2020-04-13 13:58:44 +0100

    Add ngModule directive and import FormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // STEP_02: declare property 'name' and assign a value of string 'Janka' to it
+  name = 'Janka';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// STEP_03: Add FormsModule subpackage to the module so the typescript knows
+// we're going to use FormsModule feature such as ngModule directive in our html
+// file (<input>).
+// This is only TYPESCRIPT import, it makes the module available for angular to import below
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+// STEP_04: Import FormsModule for angular
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
