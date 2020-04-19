08001f8ed47a917e8ac3adbabbe85b1a73c7d059
Janina Zylinska <janeczka@gmail.com>
2020-04-19 18:10:29 +0100

    CleanUp

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,13 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-// 01-Basics_04 IMPORTANT: You have to ahve at least one template for your component!
-// 01-Basics_04_STEP_01 Change external template file for inline template:
-  // template: `
-  //   <div>
-  //     <h3>Hi. I'm inline template</h3>
-  //   </div>
-  // `,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
@@ -21,8 +14,4 @@ export class ServersComponent implements OnInit {
 
 }
 
-// 1. External template:
-// templateUrl: './servers.component.html',
 
-// 2. Inline template:
-// template: '<app-server></app-server>',
