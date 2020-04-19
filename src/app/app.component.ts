f532c387da64e4e4471601a2fe0edd38047c22bf
Janina Zylinska <janeczka@gmail.com>
2020-04-19 17:45:44 +0100

    Basics - Inline styles in Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,15 @@ import { Component } from '@angular/core';
 // 01-Basics_01_STEP_02 here is selector that loads component to wherever it's called
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // 01-Basics_05_STEP_01 It's 'Urls' plural bc we could add pointers to other stylesheets
+  // styleUrls: ['./app.component.css', './app.component2.css']
+
+  // 01-Basics_05_STEP_02 We can also set up another property with inline styles
+  styles: [`
+    h1 {
+      color: goldenrod;
+    }
+  `]
 })
 export class AppComponent {
   title = 'Welcome to Servers!';
