21d622ba7121dca0c1000be179b1ba94fc5c0a65
Janina Zylinska <janeczka@gmail.com>
2020-04-19 18:11:34 +0100

    Basics - Component property: selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // 01-Basics_06_STEP_01 this property value is now set to same selector as we use in css,
+  // but we can use different types of selectors
+  // selector: 'app-servers',
+
+  // 01-Basics_06_STEP_02 We can use an 'attribute selector'
+  // selector: '[app-servers]',
+
+  // 01-Basics_06_STEP_03 We can use an 'class selector'
+  selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
@@ -14,4 +22,9 @@ export class ServersComponent implements OnInit {
 
 }
 
+// @Component property is a pair of "key: value", first 'key' is selector. Its
+// value is usually set to a name of a css selector:
+// 1. css selector: 'app-servers' it's used in html file as <app-servers> tag
+// 2. Attribute selector: '[app-servers]' it's used in html file as <div app-servers> attribute added to a tag
+// 3. Class selector: '.app-servers'
 
