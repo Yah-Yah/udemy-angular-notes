4a563c3de17bcc9bba91ab2f2526c38fc81f2951
Janina Zylinska <janeczka@gmail.com>
2020-04-26 22:23:55 +0100

    Basics - Input_02: Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  // STEP_05 Create new property (that will be binded to and will controll
+  // the 'disabled' attribute in html) and set it to false
+  disallowNewServer = true; // type boolean is inferred so no need to add explicit type
 
-  constructor() { }
+  // STEP_06 Add function that changes value of a property created earlier
+  // constructor is a method executed at the time that this component is created by angular
+  constructor() {
+  // STEP_07 This will mimic delay of server response in enabling button after
+  // loading the page:
+    setTimeout(() => {
+      this.disallowNewServer = false;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
