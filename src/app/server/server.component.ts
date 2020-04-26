9bec7678e52784cd13dd147bd5b0498aca803caf
Janina Zylinska <janeczka@gmail.com>
2020-04-26 22:23:26 +0100

    Basics - Input_1: String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  // STEP_02: We're going to hardcode these data outputted in html template as two different properties:
+  serverId = 10; // type: number - it oculd be assigned as "serverId: number = 10" but it's inferred (obvious by the fact of the value) so not necessary to write it explicitly;
+  serverStatus = 'offline'; // type: string
+
+  // For the string interpolation working with methods we declare methods here:
+  addition(x:any, y:number) {
+    return x+y;
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
